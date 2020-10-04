a9660f02564140967b3b9a22930b8fa720c5964c
Louis Aeilot <1329415023@qq.com>
2020-10-04 16:58:16 +0800

    Add basic features

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
+#include <array>
+
+#define DEBUG
 
 using namespace std;
 
+class Sudoku{
+public:
+    array<array<int, 9>, 9> data;
+    
+    Sudoku(){
+        
+    }
+    
+    // Utils
+    void read(){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                cin >> data.at(i).at(j);
+            }
+        }
+    }
+    void print(){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                cout << data.at(i).at(j) << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+#ifdef DEBUG
+void debug(){
+    Sudoku sudoku = Sudoku();
+    sudoku.read();
+    sudoku.print();
+}
+#endif
+
 int main(int argc, char* agrv[]){
+    std::ios::sync_with_stdio(false); // Disable Cin/Cout Sync to make the program faster
+    #ifdef DEBUG
+    debug();
+    #endif
 	return 0;
 }
 
