f073ba7630981accb2085fae33ea71c839404591
MrWillCom <mr.will.com@outlook.com>
2020-10-05 16:02:11 +0800

    Move class Sudoku to sudoku.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,76 +3,11 @@
 #include <vector>
 #include <array>
 
+#include "sudoku.hpp"
+
 #define DEBUG
 
 using namespace std;
-
-class Sudoku
-{
-public:
-    array<array<char, 9>, 9> data;
-    
-    Sudoku()
-    {
-        
-    }
-    
-    // Utils
-    void read()
-    {
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                cin >> data.at(i).at(j);
-            }
-        }
-    }
-
-    void print()
-    {
-        for(int i = 0; i < 9; i++)
-        {
-            cout << "-------------------------------------\n";
-            for(int j = 0; j < 9; j++)
-            {
-
-                if (j == 0)
-                {
-                    cout << "| ";
-                }
-                
-                cout << data.at(i).at(j) << " | ";
-            }
-            cout << endl;
-        }
-        cout << "-------------------------------------\n";
-    }
-
-    string to_string()
-    {
-        string str = "";
-
-        for(int i = 0; i < 9; i++){
-            str = str + "-------------------------------------\n";
-
-            for(int j = 0; j < 9; j++){
-
-                char current_item = data.at(i).at(j);
-
-                if (j == 0)
-                {
-                    str = str + "| ";
-                }
-
-                str = str + current_item;
-                str = str + " | ";
-            }
-            str = str + "\n";
-        }
-        str = str + "-------------------------------------\n";
-
-        return str;
-    }
-};
 
 #ifdef DEBUG
 void debug()
---
+++ src/sudoku.hpp
@@ -0,0 +1,88 @@
+#ifndef SUDOKU_HPP
+#define SUDOKU_HPP
+
+#include <iostream>
+#include <cstdio>
+#include <array>
+
+using namespace std;
+
+class Sudoku
+{
+public:
+    array<array<char, 9>, 9> data;
+    
+    Sudoku()
+    {
+        
+    }
+    
+    // Utils
+    string thick_horizontal_divider = "|===================================|\n";
+    string horizontal_divider = "|-----------------------------------|\n";
+
+    void read()
+    {
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                cin >> data.at(i).at(j);
+            }
+        }
+    }
+
+    void print()
+    {
+        cout << thick_horizontal_divider;
+        for(int i = 0; i < 9; i++)
+        {
+            if (i != 0)
+            {
+                cout << horizontal_divider;
+            }
+            
+            for(int j = 0; j < 9; j++)
+            {
+
+                if (j == 0)
+                {
+                    cout << "| ";
+                }
+                
+                cout << data.at(i).at(j) << " | ";
+            }
+            cout << endl;
+        }
+        cout << thick_horizontal_divider;
+    }
+
+    string to_string()
+    {
+        string str = thick_horizontal_divider;
+
+        for(int i = 0; i < 9; i++){
+            if (i != 0)
+            {
+                str = str + horizontal_divider;
+            }
+
+            for(int j = 0; j < 9; j++){
+
+                char current_item = data.at(i).at(j);
+
+                if (j == 0)
+                {
+                    str = str + "| ";
+                }
+
+                str = str + current_item;
+                str = str + " | ";
+            }
+            str = str + "\n";
+        }
+        str = str + thick_horizontal_divider;
+
+        return str;
+    }
+};
+
+#endif
