9ad3038243300817504182155accf961627db393
MrWillCom <mr.will.com@outlook.com>
2020-10-05 15:41:41 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,33 @@
 
 using namespace std;
 
-class Sudoku{
+class Sudoku
+{
 public:
-    array<array<int, 9>, 9> data;
+    array<array<char, 9>, 9> data;
     
-    Sudoku(){
+    Sudoku()
+    {
         
     }
     
     // Utils
-    void read(){
+    void read()
+    {
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 cin >> data.at(i).at(j);
             }
         }
     }
-    void print(){
-        for(int i = 0; i < 9; i++){
+
+    void print()
+    {
+        for(int i = 0; i < 9; i++)
+        {
             cout << "-------------------------------------\n";
-            for(int j = 0; j < 9; j++){
+            for(int j = 0; j < 9; j++)
+            {
 
                 if (j == 0)
                 {
@@ -39,17 +46,47 @@ public:
         }
         cout << "-------------------------------------\n";
     }
+
+    string to_string()
+    {
+        string str = "";
+
+        for(int i = 0; i < 9; i++){
+            str = str + "-------------------------------------\n";
+
+            for(int j = 0; j < 9; j++){
+
+                char current_item = data.at(i).at(j);
+
+                if (j == 0)
+                {
+                    str = str + "| ";
+                }
+
+                str = str + current_item;
+                str = str + " | ";
+            }
+            str = str + "\n";
+        }
+        str = str + "-------------------------------------\n";
+
+        return str;
+    }
 };
 
 #ifdef DEBUG
-void debug(){
+void debug()
+{
     Sudoku sudoku = Sudoku();
     sudoku.read();
     sudoku.print();
+    cout << "to_string()" << endl;
+    cout << sudoku.to_string();
 }
 #endif
 
-int main(int argc, char* agrv[]){
+int main(int argc, char* agrv[])
+{
     std::ios::sync_with_stdio(false); // Disable Cin/Cout Sync to make the program faster
     #ifdef DEBUG
     debug();
