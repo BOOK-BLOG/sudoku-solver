a1691aaffbf57bb6872cf29e0e47644da62078b3
Clang Code Formatter <nobody@nobody.nobody>
2020-10-06 03:03:48 +0000

    Clang Code Format

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <cstdio>
-#include <vector>
 #include <array>
+#include <cstdio>
+#include <iostream>
+#include <vector>
 
 #include "sudoku.h"
 
@@ -10,25 +10,21 @@
 using namespace std;
 
 #ifdef DEBUG
-void debug()
-{
-    Sudoku sudoku = Sudoku();
-    sudoku.command_line_input();
-    sudoku.print();
-    cout << endl << "to_string()" << endl;
-    cout << sudoku.to_string();
+void debug() {
+  Sudoku sudoku = Sudoku();
+  sudoku.command_line_input();
+  sudoku.print();
+  cout << endl << "to_string()" << endl;
+  cout << sudoku.to_string();
 }
 #endif
 
-int main(int argc, char* argv[])
-{
-    std::ios::sync_with_stdio(false); // Disable Cin/Cout Sync to make the program faster
-    #ifdef DEBUG
-    debug();
-    #endif
+int main(int argc, char *argv[]) {
+  std::ios::sync_with_stdio(
+      false); // Disable Cin/Cout Sync to make the program faster
+#ifdef DEBUG
+  debug();
+#endif
 
-    
-
-    return 0;
+  return 0;
 }
-
--- src/sudoku.h
+++ src/sudoku.h
@@ -1,81 +1,65 @@
 #ifndef SUDOKU_H
 #define SUDOKU_H
 
-#include <iostream>
-#include <cstdio>
 #include <array>
+#include <cstdio>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
-class Sudoku
-{
+class Sudoku {
 public:
-    array<array<char, 9>, 9> data;
+  array<array<char, 9>, 9> data;
 
-    Sudoku()
-    {
-        
+  Sudoku() {}
+
+  Sudoku solve() {}
+
+  // Utils
+  string thick_horizontal_divider = "|===================================|\n";
+  string horizontal_divider = "|-----------------------------------|\n";
+
+  void command_line_input() {
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        cin >> data.at(i).at(j);
+      }
     }
+  }
 
-    Sudoku solve()
-    {
-        
-    }
+  void read_file(string path) {
+    ifstream file;
+    file.open(path, ios::out | ios::in);
+  }
 
-    // Utils
-    string thick_horizontal_divider = "|===================================|\n";
-    string horizontal_divider = "|-----------------------------------|\n";
+  void print() { cout << to_string(); }
 
-    void command_line_input()
-    {
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                cin >> data.at(i).at(j);
-            }
+  string to_string() {
+    string str = thick_horizontal_divider;
+
+    for (int i = 0; i < 9; i++) {
+      if (i != 0) {
+        str = str + horizontal_divider;
+      }
+
+      for (int j = 0; j < 9; j++) {
+
+        char current_item = data.at(i).at(j);
+
+        if (j == 0) {
+          str = str + "| ";
         }
+
+        str = str + current_item;
+        str = str + " | ";
+      }
+      str = str + "\n";
     }
+    str = str + thick_horizontal_divider;
 
-    void read_file(string path)
-    {
-        ifstream file;
-        file.open(path, ios::out | ios::in);
-        
-    }
-
-    void print()
-    {
-        cout << to_string();
-    }
-
-    string to_string()
-    {
-        string str = thick_horizontal_divider;
-
-        for(int i = 0; i < 9; i++){
-            if (i != 0)
-            {
-                str = str + horizontal_divider;
-            }
-
-            for(int j = 0; j < 9; j++){
-
-                char current_item = data.at(i).at(j);
-
-                if (j == 0)
-                {
-                    str = str + "| ";
-                }
-
-                str = str + current_item;
-                str = str + " | ";
-            }
-            str = str + "\n";
-        }
-        str = str + thick_horizontal_divider;
-
-        return str;
-    }
+    return str;
+  }
 };
 
 #endif
